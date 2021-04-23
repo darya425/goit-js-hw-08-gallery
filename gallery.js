466e09358e1f56b963d17b3a7d8d9a7206e72b1a
darya425 <dasha425@mail.ru>
2021-04-23 17:27:08 +0200

    fixed slider

--- gallery.js
+++ gallery.js
@@ -8,159 +8,127 @@ const btnModal = document.querySelector('[data-action="close-lightbox"]');
 
 const itemsMarkup = createGalleryItemsMarkup(itemsGallery); //дала разметке имя
 
-gallery.insertAdjacentHTML('beforeend', itemsMarkup); // запарсила строку в картинки
-gallery.addEventListener('click', onItenContainerClick); // слушатель для открытия модалки
-btnModal.addEventListener('click', removeActiveClass); // слушатель для закрытия модалки
-backdrop.addEventListener('click', removeActiveClass);
-
-
 //создала разметку
 function createGalleryItemsMarkup(items) {
-  return items.map(({ preview, original, description }) => {
-    return `<li class="gallery__item">
-                <a
-                  class="gallery__link"
-                  href="${original}"
-                >
-                  <img
-                    class="gallery__image"
-                    src="${preview}"
-                    data-source="${original}"
-                    alt="${description}"
-                  />
-                </a>
-              </li>`
-  }).join('');
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                  <a
+                    class="gallery__link"
+                    href="${original}"
+                  >
+                    <img
+                      class="gallery__image"
+                      src="${preview}"
+                      data-source="${original}"
+                      alt="${description}"
+                    />
+                  </a>
+                </li>`
+      })
+    .join('');
 }
-
-// мероприятие при открытии модалки
-function onItenContainerClick(evt) {
-  evt.preventDefault();
-  const isItemClick = evt.target.classList.contains('gallery__image');
-  
-  if (!isItemClick) {
-    return;
-  }
-
-  const galleryItem = evt.target;
-  const imgLink = galleryItem.dataset.source;
-  const imgDescr = galleryItem.alt;
-
-  addActiveClass(modal);
-  setItemLink(imgLink, imgDescr);
-}
-
-// добавила класс
-function addActiveClass(item) {
-  window.addEventListener('keydown', onEscCloseModal); // повесила событие клавиши только при открытой модалке
-  backRef.addEventListener('click', onClickLeftBtn); // слушатель для перемотки влево
-  window.addEventListener('keydown', onLeftKeyClick); 
-  nextRef.addEventListener('click', onClickRightBtn); // слушатель для перемотки вправо
-  window.addEventListener('keydown', onRightKeyClick);
-
-  item.classList.add('is-open');
-}
-
-//подмена линка
-function setItemLink(link, descr) {
-  modalImg.src = link;
-  modalImg.alt = descr;
-}
-
-// удалила класс и очистила ссылки
-function removeActiveClass() {
-    window.removeEventListener('keydown', onEscCloseModal); // сняла событие клавиши после закрытия модалки
-    backRef.removeEventListener('click', onClickLeftBtn); // слушатель для перемотки влево
-    window.removeEventListener('keydown', onLeftKeyClick); 
-    nextRef.removeEventListener('click', onClickRightBtn); // слушатель для перемотки вправо
-    window.removeEventListener('keydown', onRightKeyClick);
-
-    const currentActiveModal = document.querySelector('.js-lightbox.is-open'); // нашла ситуацию, когда класс нужно убирать
-    
-    if (currentActiveModal) {
-      currentActiveModal.classList.remove('is-open'); // в этом случае и удалила
-      modalImg.src = "";
-      modalImg.alt = "";
-    }
-}
-
-function onEscCloseModal(evt) { // повесила событие только на ESC
-    
-    if (evt.code === 'Escape') {
-      removeActiveClass(); 
-    }
-}
-
-
-// Карусель
 
 modal.insertAdjacentHTML('beforeend',
   `<button type="button" class="lightbox__button left" data-action="next-lightbox"></button> 
   <button type="button" class="lightbox__button right" data-action="next-lightbox"></button> 
 `);
 
-
 const backRef = document.querySelector('.lightbox__button.left'); // кнопка влево
 const nextRef = document.querySelector('.lightbox__button.right'); // кнопка вправо
-const imagesRef = document.querySelectorAll('.gallery__image'); // все фотки
 
-let currentImg = 0;
+gallery.insertAdjacentHTML('beforeend', itemsMarkup); // запарсила строку в картинки
+gallery.addEventListener('click', openModal); // слушатель для открытия модалки
+btnModal.addEventListener('click', onCloseModal); // слушатель для закрытия модалки
+backdrop.addEventListener('click', onBackdropClick); // слушатель закрытия модалки по клику в бэкдроп
+
+// мероприятие при открытии модалки
+function openModal(evt) {
+    evt.preventDefault(); // убрала поведение по умолчанию
+    window.addEventListener('keydown', onPressEsc); // закрытие модалки на ESC работает только при открытой модалке
+    window.addEventListener('keydown', onArrowRight); // при открытой модалке клик по правой стрелке на клаве
+    window.addEventListener('keydown', onArrowLeft);
+    backRef.addEventListener('click', onLeftNext(itemsGallery)); // слушатель для перемотки влево
+    nextRef.addEventListener('click', onRightNext(itemsGallery)); // слушатель для перемотки вправо
+
+    if (evt.target === evt.currentTarget) { // если таргет не таргет, то не работает
+        return;
+    }
+
+    modal.classList.add('is-open'); // добавила класс модалки
+    modalImg.src = evt.target.getAttribute('data-source');
+    modalImg.alt = evt.target.alt;
+}
+
+// закрытие модалки
+function onCloseModal() {
+    window.removeEventListener('keydown', onPressEsc); // сняла событие клавиши после закрытия модалки
+    window.removeEventListener('keydown', onArrowRight); // при закрытой модалке клик по правой стрелке на клавене работает
+    window.removeEventListener('keydown', onArrowLeft);
+    backRef.removeEventListener('click', onLeftNext(itemsGallery)); // слушатель для перемотки влево
+    nextRef.removeEventListener('click', onRightNext(itemsGallery)); // слушатель для перемотки вправо
+
+    modal.classList.remove('is-open'); // удалила класс модалки
+    modalImg.src = "";
+    modalImg.alt = "";  
+}
+
+// Закрытие по клавише ESC
+function onPressEsc(evt) { // повесила событие только на ESC
+    if (evt.code === 'Escape') {
+      onCloseModal(); 
+    }
+}
+
+// Закрытие по клику на бекдроп
+function onBackdropClick(evt) {
+    if (evt.target === evt.currentTarget) {
+            onCloseModal();
+    }
+}
+
+// Карусель
+
+
 
 // стучим по кнопке вправо
-function onRightKeyClick(evt) {
+function onArrowRight(evt) {
   if (evt.code === "ArrowRight") {
-    onClickRightBtn()
+    onRightNext(itemsGallery)
   }
 }
 
 // стучим по кнопке влево
-function onLeftKeyClick(evt) {
+function onArrowLeft(evt) {
   if (evt.code === "ArrowLeft") {
-    onClickLeftBtn()
+    onLeftNext(itemsGallery)
   }
 }
 
 
 // правая стрелка
-function onClickRightBtn() {
-  
-  if (currentImg === imagesRef.length - 1) {
-    currentImg = 0;
-  } else {
-    currentImg += 1;
-  }
-
-  modalImg.src = imagesRef[currentImg].dataset.source;
+function onRightNext(items) {
+    const activeImage = items.find(img => img.original === modalImg.src);
+    let index = activeImage ? items.indexOf(activeImage) : 0;
+    if (index < items.length - 1) {
+        index += 1;
+    } else {
+        index = 0;
+    }
+    modalImg.src = items[index].original;
+    modalImg.alt = items[index].alt;
 }
 
 // левая стрелка
-function onClickLeftBtn() {
-  
-  if (currentImg === 0) {
-    currentImg = imagesRef.length - 1;
-  } else {
-    currentImg -= 1;
-  }
-
-  modalImg.src = imagesRef[currentImg].dataset.source;
+function onLeftNext(items) {
+    const activeImage = items.find(img => img.original === modalImg.src);
+    let index = activeImage ? items.indexOf(activeImage) : items.length - 1;
+    if (index === 0) {
+        index = items.length - 1;
+    } else {
+        index -= 1;
+    }
+    modalImg.src = items[index].original;
+    modalImg.alt = items[index].alt;
 }
 
-// В ЧЕМ ПРОБЛЕМА ЭТОГО КОДА?
-
-// // правая кнопка
-// function onClickRightBtn() {
-//   currentImg += 1;
-//   if (currentImg === (imagesRef.length - 1)) {
-//     nextRef.disabled = true;
-//   }
-//   modalImg.src = imagesRef[currentImg].dataset.source;
-// }
-
-// // левая кнопка
-// function onClickLeftBtn() {
-//   currentImg -= 1;
-//   if (currentImg === 0) {
-//     backRef.disabled = true;
-//   }
-//   modalImg.src = imagesRef[currentImg].dataset.source;
-// }
